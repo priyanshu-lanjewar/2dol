d8e2fa7fd0d0ddc2b3e92e0442cb99ac8c3a59fd
priyanshu-lanjewar <priyanshu.lanjewar@yahoo.com>
2023-04-27 22:40:56 +0530

    enhanced purge

--- cmd/purge.go
+++ cmd/purge.go
@@ -4,10 +4,8 @@ Copyright © 2023 PRIYANSHU LANJEWAR @ PRIYANSHU.LANJEWAR@YAHOO.COM
 package cmd
 
 import (
-	"github.com/priyanshu-lanjewar/2dol/pkg/common"
 	"github.com/priyanshu-lanjewar/2dol/pkg/purge"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 // purgeCmd represents the purge command
@@ -16,12 +14,10 @@ var purgeCmd = &cobra.Command{
 	Short: "Deletes the TO-DO list",
 	Aliases: []string{"p","P"},
 	Run: func(cmd *cobra.Command, args []string) {
-		viper.BindPFlags(cmd.Flags())
-		common.ListName = args[0]
 		purge.Execute()
 	},
 }
 
 func init() {
 	
-	}
+}
--- pkg/purge/exec.go
+++ pkg/purge/exec.go
@@ -3,18 +3,51 @@ package purge
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
+	"github.com/AlecAivazis/survey/v2"
 	"github.com/priyanshu-lanjewar/2dol/pkg/common"
 )
 
 func Execute() {
-	common.ListFilePath = fmt.Sprintf("%s/.%s", common.UserToDoListDirectoryPath, common.ListName)
-	if _, common.Error = os.Stat(common.ListFilePath); os.IsNotExist(common.Error) {
-		common.LogMsg(fmt.Sprintf("The List with Name %s doesnt Exists.",common.ListName))
+	c :=0
+	ListItemInp := make([]string,0)
+	ListItemOut := make([]string,0)
+	common.Error = filepath.Walk(common.UserToDoListDirectoryPath,func(path string, info os.FileInfo,err error)error{
+		if err != nil{
+			return err
+		}
+
+		if !info.IsDir(){
+			if info.Name() != ".conf"{
+				ListItemInp= append(ListItemInp,info.Name()[1:])
+				c+=1
+			}
+		}
+		return nil
+	})
+
+	common.LogErr(common.Error)
+	if c==0{
+		common.LogMsg("No List Exists")
 	} else {
-		common.Error = os.Remove(common.ListFilePath)
-		common.LogErr(common.Error)
-		common.LogMsg("List Deleted Successfully")
-		common.DecConfCount()
+		prompt := &survey.MultiSelect{
+			Message: "Select Lists to Delete:",
+			Options: ListItemInp,
+		}
+		survey.AskOne(prompt,&ListItemOut)
+	}
+
+	for _, item := range ListItemOut{
+		common.ListName = item
+		common.ListFilePath = fmt.Sprintf("%s/.%s", common.UserToDoListDirectoryPath, common.ListName)
+		if _, common.Error = os.Stat(common.ListFilePath); os.IsNotExist(common.Error) {
+			common.LogMsg(fmt.Sprintf("The List with Name %s doesnt Exists.",common.ListName))
+		} else {
+			common.Error = os.Remove(common.ListFilePath)
+			common.LogErr(common.Error)
+			common.LogMsg(fmt.Sprintf("List Deleted Successfully: %s",common.ListName))
+			common.DecConfCount()
+		}
 	}
 }
