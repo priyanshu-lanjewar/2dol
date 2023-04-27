d8aa7d35335814404a7424cc2695a0b66ddc71eb
priyanshu-lanjewar <priyanshu.lanjewar@yahoo.com>
2023-04-27 22:27:18 +0530

    enhanced creat

--- cmd/create.go
+++ cmd/create.go
@@ -4,10 +4,8 @@ Copyright © 2023 PRIYANSHU LANJEWAR @ PRIYANSHU.LANJEWAR@YAHOO.COM
 package cmd
 
 import (
-	"github.com/priyanshu-lanjewar/2dol/pkg/common"
 	"github.com/priyanshu-lanjewar/2dol/pkg/create"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 // createCmd represents the create command
@@ -17,8 +15,6 @@ var createCmd = &cobra.Command{
 	Aliases: []string{"c","C"},
 	
 	Run: func(cmd *cobra.Command, args []string) {
-		viper.BindPFlags(cmd.Flags())
-		common.ListName = args[0]
 		create.Execute()
 	},
 }
--- pkg/create/exec.go
+++ pkg/create/exec.go
@@ -4,10 +4,29 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/AlecAivazis/survey/v2"
 	"github.com/priyanshu-lanjewar/2dol/pkg/common"
 )
 
 func Execute() {
+	qs := []*survey.Question{
+		{
+			Name: "list",
+			Prompt: &survey.Input{
+				Message: "Enter Name of New List:",
+			},
+			Validate: survey.Required,
+			Transform: survey.Title,
+		},
+	}
+
+	rs := struct{
+		List string `survey:"list"`
+	}{}
+
+	common.Error = survey.Ask(qs,&rs)
+	common.LogErr(common.Error)
+	common.ListName = rs.List
 	common.ListFilePath = fmt.Sprintf("%s/.%s",common.UserToDoListDirectoryPath,common.ListName)
 	if _, common.Error = os.Stat(common.ListFilePath); os.IsNotExist(common.Error) {
 		common.ListFile, common.Error = os.Create(common.ListFilePath) 
@@ -16,6 +35,7 @@ func Execute() {
 		common.ListFile.Close()
 		common.IncConfCount()
 		common.UpdateConfListName(common.ListName)
+		common.LogMsg(fmt.Sprintf("Selected List %s.",common.ListName))
 	} else {
 		common.LogMsg("A List With Same Name Already Exists.")
 	}
